1f318b4ea97d1bbb32296747a5b80f13e5d34637
rafaelrch <rafaelrocha250304@gmail.com>
2024-11-22 10:11:17 -0300

    erro no video

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Element } from "react-scroll";
 import video1 from '../assets/video1.mp4';
-import video2 from '../assets/video2.mp4';
 import video3 from '../assets/video3.mp4';
 
 function Hero() {
